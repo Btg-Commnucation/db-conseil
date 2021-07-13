5788e0aec7620065ca65ff405738c5484ed59909
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-07-13 14:56:21 +0200

    debut

--- functions.php
+++ functions.php
@@ -12,3 +12,8 @@ function pdw_load_stylesheets_and_scripts() {
     // wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array(), '1.0', true);
 }
 add_action( 'wp_enqueue_scripts', 'pdw_load_stylesheets_and_scripts' );
+
+
+register_nav_menus( array(
+    'main' => 'Menu principal',
+) );
--- header.php
+++ header.php
@@ -7,5 +7,23 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(  ); ?>>
-
     <?php wp_body_open(); ?>
+    <header>
+        <img src="<?php echo get_template_directory_uri(); ?>/img/top-logo.svg" alt="DB Conseil, Un accompagnement sur-mesure">
+        <div class="main-menu__container">
+            <?php wp_nav_menu( array(
+                'theme_location' => 'main',
+                'container' => 'nav',
+                'menu_id' => 'main-menu',
+                'fallback_cb' => false,
+                'items_wrap' => '<div class="menu-btn">
+                    <div class="menu-btn__burger">
+                    </div>
+                </div><ul id="%1$s" class="%2$s" hidden>%3$s</ul>',
+            ) ); ?>
+        </div>
+        <div class="form-button__container">
+            <a href="#">Je postule</a>
+            <a href="#">Je recrute</a>
+        </div>
+    </header>
