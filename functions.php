65d2db23fbc432d69310bd55973650af1deed4d2
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-07-13 14:07:44 +0200

    Init finalize

--- functions.php
+++ functions.php
@@ -6,3 +6,9 @@ add_theme_support( 'post_thumbnails' );
 
 // Ajouter automatiquement le titre du site dans l'en-tête du site
 add_theme_support( 'title-tag' );
+
+function pdw_load_stylesheets_and_scripts() {
+    wp_enqueue_style( 'style', get_template_directory_uri() . '/css/style.css', array(), '1.0');
+    // wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array(), '1.0', true);
+}
+add_action( 'wp_enqueue_scripts', 'pdw_load_stylesheets_and_scripts' );
