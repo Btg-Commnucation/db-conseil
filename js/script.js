68d2fbbbfa78abf289e30324885e7247acf9a637
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-07-13 14:58:04 +0200

    debut

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let windowWidth = window.innerWidth;
+if (windowWidth <= 700) {
+  const menuBtn = document.querySelector(".menu-btn");
+  const mainMenu = document.getElementById("main-menu");
+  let menuOpen = false;
+  menuBtn.addEventListener("click", () => {
+    if (!menuOpen) {
+      menuBtn.classList.add("open");
+      menuOpen = true;
+      mainMenu.hidden = !mainMenu.hidden;
+    } else {
+      menuBtn.classList.remove("open");
+      menuOpen = false;
+      mainMenu.hidden = !mainMenu.hidden;
+    }
+  });
+
+  const menuBtnBurger = document.querySelector(".menu-btn__burger");
+}
