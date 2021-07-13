d063ad4bb44dba70c6ee3b947221ca59cfedd8d4
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-07-13 16:04:05 +0200

    continuing footer

--- footer.php
+++ footer.php
@@ -1,15 +1,16 @@
 <footer>
-    <section class="hero-footer">
-        <div>
-            <img src="<?php echo get_template_directory_uri(); ?>/img/top-logo.svg" alt="DB conseil, un accompagnement sur-mesure">
-            <?php dynamic_sidebar( 'footer-address' ); ?>
-        </div>
-        <div></div>
-        <div></div>
-        <div></div>
-    </section>
-    <section class="mentions"></section>
-
+    <div class="container">
+        <section class="hero-footer">
+            <div class="widget-container">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/top-logo.svg" alt="DB conseil, un accompagnement sur-mesure">
+                <?php dynamic_sidebar( 'footer-address' ); ?>
+            </div>
+            <div class="widget-container"></div>
+            <div class="widget-container"></div>
+            <div class="widget-container"></div>
+        </section>
+        <section class="mentions"></section>
+    </div>
 </footer>
 
 <?php wp_footer();  ?>
