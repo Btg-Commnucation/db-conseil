e50f1a737dc35144e64325282f2d9c3875d9cd7a
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-07-15 11:43:19 +0200

    back image

--- template-list.php
+++ template-list.php
@@ -23,22 +23,25 @@ get_header();
     <script type="text/x-template" id="home">
         <div>
             <section class="hero-banner">
+                <div class="background-image"></div>
                 <div class="gradient">
                     <div class="container">
                         <h1><?php the_field('main_title'); ?></h1>
-                        <form action="">
+                        <div class="job-container">
                             <h2>Je trouve mon poste</h2>
-                            <select v-model="searchCategorie" name="categorie" id="categorie" aria-label="Quelle catégorie de poste ?">
-                                <option value="">Catégorie</option>
-                                <option v-for="categorie in filteredCategory" v-bind:value="categorie">{{categorie}}</option>
-                            </select>
-                            <select v-model="searchRegion" name="region" id="region" aria-label="Dans quelle région recherchez-vous ?">
-                                <option value="">Région</option>
-                                <option v-for="region in filteredRegion" v-bind:value="region">{{region}}</option>
-                            </select>
-                            <input v-model="searchJobType" type="text" name="post-type" id="post-type" placeholder="Intitulé du poste" aria-label="Intitulé du poste">
-                            <router-link to="{ name: 'Resultats', params: { searchCaterogie, searchRegion, searchJobType } }"></router-link>
-                        </form>
+                            <form action="">
+                                <select v-model="searchCategorie" name="categorie" id="categorie" aria-label="Quelle catégorie de poste ?">
+                                    <option value="">Catégorie</option>
+                                    <option v-for="categorie in filteredCategory" v-bind:value="categorie">{{categorie}}</option>
+                                </select>
+                                <select v-model="searchRegion" name="region" id="region" aria-label="Dans quelle région recherchez-vous ?">
+                                    <option value="">Région</option>
+                                    <option v-for="region in filteredRegion" v-bind:value="region">{{region}}</option>
+                                </select>
+                                <input v-model="searchJobType" type="text" name="post-type" id="post-type" placeholder="Intitulé du poste" aria-label="Intitulé du poste">
+                                <router-link to="{ name: 'Resultats', params: { searchCaterogie, searchRegion, searchJobType } }">Rechercher</router-link>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </section>
