6cf8c5297561277bcb58f328cb7b8be1a645b124
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-07-15 09:40:00 +0200

    startgin

--- template-list.php
+++ template-list.php
@@ -5,5 +5,13 @@ Template Name: Liste de job;
 */
 get_header();
 ?>
-
+<main class="list-page">
+    <div class="app">
+        <div class="display-none">
+            <router-link to="/"></router-link>
+            <router-link to="/Resultat"></router-link>
+            <router-link to="Description"></router-link>
+        </div>
+    </div>
+</main>
 <?php get_footer(); ?>
