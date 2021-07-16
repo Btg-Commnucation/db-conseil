ec254d7f5631a1ebdbf427d635c1c5281f211121
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-07-16 14:33:03 +0200

    contact debut

--- template-contact.php
+++ template-contact.php
@@ -5,16 +5,18 @@ Template Name: Contact
 
 */
 get_header();
+$main_image = get_field('image');
+$logo_image = get_field('logo');
 ?>
 <main class="contact">
     <section class="hero-banner">
         <div class="container">
             <h1><?php the_title(); ?></h1>
             <div class="adresse">
-                <img src="" alt="">
+                <img src="<?php echo esc_url($main_image['url']); ?>" alt="<?php echo esc_attr($main_image['alt']); ?>">
                 <div class="adresse-detail">
-                    <img src="" alt="">
-                    <?php the_field('adresse_contact') ?>
+                    <img src="<?php echo esc_url($logo_image['url']); ?>" alt="<?php echo esc_attr($logo_image['alt']); ?>">
+                    <?php the_field('adresse_contract') ?>
                 </div>
             </div>
         </div>
