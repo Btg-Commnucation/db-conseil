f95a87933b32937cce21e5a8010cf5868b7f97ca
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-07-19 14:36:21 +0200

    ACF field Cabinet top done

--- page.php
+++ page.php
@@ -2,7 +2,7 @@
 <main class="cabinet">
     <section class="hero-banner">
         <div class="container">
-            <div class="header">
+            <div class="article-container">
                 <div class="left-side">
                     <h1><?php the_title(); ?></h1>
                     <h2><?php the_field('cabinet'); ?></h2>
@@ -14,6 +14,15 @@
                 <div class="right-side">
                     <?php $image_premiere = get_field('image_first'); ?>
                     <img src="<?php echo esc_url($image_premiere['url']); ?>" alt="<?php echo esc_attr($image_premiere['alt']); ?>">
+                </div>
+            </div>
+            <div class="article-container bottom-article">
+                <div class="left-side">
+                    <?php $image_second = get_field('image_second'); ?>
+                    <img src="<?php echo esc_url($image_second['url']); ?>" alt="<?php echo esc_attr($image_second['alt']); ?>">
+                </div>
+                <div class="right-side">
+                    <?php the_field('texte_long_second'); ?>
                 </div>
             </div>
         </div>
