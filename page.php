cafce06e82559d0648ccee99c4cf8dc7e5eb60b2
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-07-19 13:45:49 +0200

    Le cabinet

---
+++ page.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+<main>
+    <section class="hero-banner">
+        <div class="header">
+            <div class="left-side">
+                <h1><?php the_title(); ?></h1>
+                <h2><?php the_field('cabinet'); ?></h2>
+                <div class="short-text"><?php the_field('texte_court'); ?></div>
+                <div class="paragraphe">
+                    <?php the_field('texte_long_first'); ?>
+                </div>
+            </div>
+            <div class="right-side">
+                <?php $image_premiere = get_field('image_first'); ?>
+                <img src="<?php echo esc_url($image_premiere['url']); ?>" alt="<?php echo esc_attr($image_premiere['alt']); ?>">
+            </div>
+        </div>
+    </section>
+</main>
+<?php get_footer(); ?>
