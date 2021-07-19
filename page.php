c5d7a7f1541290b11a81eec78ff3de1d0a322600
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-07-19 13:52:27 +0200

    Cabinet first style

--- page.php
+++ page.php
@@ -1,18 +1,20 @@
 <?php get_header(); ?>
-<main>
+<main class="cabinet">
     <section class="hero-banner">
-        <div class="header">
-            <div class="left-side">
-                <h1><?php the_title(); ?></h1>
-                <h2><?php the_field('cabinet'); ?></h2>
-                <div class="short-text"><?php the_field('texte_court'); ?></div>
-                <div class="paragraphe">
-                    <?php the_field('texte_long_first'); ?>
+        <div class="container">
+            <div class="header">
+                <div class="left-side">
+                    <h1><?php the_title(); ?></h1>
+                    <h2><?php the_field('cabinet'); ?></h2>
+                    <div class="short-text"><?php the_field('texte_court'); ?></div>
+                    <div class="paragraphe">
+                        <?php the_field('texte_long_first'); ?>
+                    </div>
                 </div>
-            </div>
-            <div class="right-side">
-                <?php $image_premiere = get_field('image_first'); ?>
-                <img src="<?php echo esc_url($image_premiere['url']); ?>" alt="<?php echo esc_attr($image_premiere['alt']); ?>">
+                <div class="right-side">
+                    <?php $image_premiere = get_field('image_first'); ?>
+                    <img src="<?php echo esc_url($image_premiere['url']); ?>" alt="<?php echo esc_attr($image_premiere['alt']); ?>">
+                </div>
             </div>
         </div>
     </section>
