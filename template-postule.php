3e0a13035cd3855a198cbd42cb621c455277b12d
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-07-20 10:36:38 +0200

    postule page

---
+++ template-postule.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<main class="postule-recrute">
+    <section class="hero-banner">
+        <div class="container">
+            <h1><?php the_title(); ?></h1>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
