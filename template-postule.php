ce25fc7285e37b5e24050c2538c17c3c32be8ad1
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-07-20 10:49:17 +0200

    je postule

--- template-postule.php
+++ template-postule.php
@@ -6,6 +6,37 @@
             <h1><?php the_title(); ?></h1>
         </div>
     </section>
+    <div id="postule">
+        <div class="display-none">
+            <router-link to="/">
+                <p>Home</p>
+            </router-link>
+            <router-link to="/Resultats">
+                <p>Rechercher</p>
+            </router-link>
+        </div>
+    </div>
+
+
+
+    <script type="text/x-template" id="home">
+        <section class="job">
+            <div class="container">
+                <h2><?php the_field('titre_carte'); ?></h2>
+                <div class="card-container">
+                    <div v-for='job in slicePost' class="card">
+                        <strong>{{job.gender}}</strong>
+                        <div class="card-detail">
+                            <p class="categorie">{{job.race}}</p>
+                            <h3>{{job.name}}</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra mi vitae eleifend efficitur. Nullam eu ipsum libero. Fusce vitae augue eu odio porta sollicitudin ut non dui. Vivamus mollis diam vel diam consequat, quis posuere felis suscipit.</p>
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </script>
 </main>
 
 <?php get_footer(); ?>
