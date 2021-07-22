c91373e49da32aaeae5c8cb036a726b954543fba
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-07-22 14:01:50 +0200

    Image bottom done

--- layout/description-front.php
+++ layout/description-front.php
@@ -137,6 +137,11 @@
             </form>
         </div>
     </section>
+    <section class="photo-bot">
+        <div class="gradient-bottom"></div>
+        <img src="<?php echo get_template_directory_uri(); ?>/img/bottom-img.jpg" alt="DB Conseils cabinet de recrutement">
+        <div class="gradient-second"></div>
+    </section>
 </div>
 
 </script>
