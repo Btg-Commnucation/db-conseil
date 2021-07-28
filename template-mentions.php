e55a7ec63d2b6bcb20d8b483568c65c0c531342d
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-07-28 15:44:22 +0200

    Template mentions legales

---
+++ template-mentions.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Template Name: Mentions légales
+*/
+?>
+
+
+<?php get_header(); ?>
+<main class="cabinet">
+        <div class="container">
+            <h1><?php the_title(); ?></h1>
+            <?php the_content(); ?>
+        </div>
+</main>
+<?php get_footer(); ?>
