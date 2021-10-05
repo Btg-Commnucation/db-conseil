1bb8fb2fe627e3caa9dcb19cd131ca900d375a82
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-10-05 10:46:26 +0200

    webpack working

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ path = require("path");
 module.exports = {
   mode: "development",
   entry: {
-    script: path.resolve(__dirname, js / script.js),
+    script: path.resolve(__dirname, "js/script.js"),
   },
   output: {
     path: path.resolve(__dirname, "dist"),
